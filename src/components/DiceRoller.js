e6b946cb3b69b3885d84c7ce4a3e8c39816ed0c8
Tyler Furgalack <104029862+tylerfurgalack@users.noreply.github.com>
2023-05-09 12:57:56 -0400

    Very basic dice funtionality

---
+++ src/components/DiceRoller.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import './DiceRoller.css';
+
+function DiceRoller() {
+  const [dice1, setDice1] = useState(1);
+  const [dice2, setDice2] = useState(1);
+
+  const rollDice = () => {
+    setDice1(Math.floor(Math.random() * 6) + 1);
+    setDice2(Math.floor(Math.random() * 6) + 1);
+  };
+
+  return (
+    <div>
+      <h1>Dice Roller</h1>
+      <div>
+        <div className={`dice die-${dice1}`}></div>
+        <div className={`dice die-${dice2}`}></div>
+      </div>
+      <button onClick={rollDice}>Roll Dice</button>
+    </div>
+  );
+}
+
+export default DiceRoller;
