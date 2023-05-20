b1f27b2e4f3c7221d51676962493ac2bea20ed6b
Tyler Furgalack <104029862+tylerfurgalack@users.noreply.github.com>
2023-05-20 00:08:27 -0400

    persistant state for which nums have been click

--- src/components/DiceRoller.js
+++ src/components/DiceRoller.js
@@ -5,21 +5,30 @@ import LettersNumbers from './LettersNumbers';
 function DiceRoller() {
   const [dice1, setDice1] = useState(1);
   const [dice2, setDice2] = useState(1);
+  const sum = dice1 + dice2;
+  const [choice, setChoice] = useState(null);
 
   const rollDice = () => {
     setDice1(Math.floor(Math.random() * 6) + 1);
     setDice2(Math.floor(Math.random() * 6) + 1);
+    setChoice(null); // Reset choice when dice values change
   };
 
-  const sum = dice1 + dice2;
+  const handleChoice = (num) => {
+    setChoice(num);
+  };
 
   return (
     <div>
-      <div>
-        <LettersNumbers dice1={dice1} dice2={dice2} sum={sum} />
-        <div className={`dice die-${dice1}`}></div>
-        <div className={`dice die-${dice2}`}></div>
-      </div>
+      <LettersNumbers
+        dice1={dice1}
+        dice2={dice2}
+        sum={sum}
+        choice={choice}
+        handleChoice={handleChoice}
+      />
+      <div className={`dice die-${dice1}`}></div>
+      <div className={`dice die-${dice2}`}></div>
       <button onClick={rollDice}>Roll Dice</button>
     </div>
   );
--- src/components/LettersNumbers.js
+++ src/components/LettersNumbers.js
@@ -1,18 +1,37 @@
+import React, { useState } from 'react';
 import './LettersNumbers.css';
 
 const LettersNumbers = ({ dice1, dice2, sum }) => {
   const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const [selectedNumbers, setSelectedNumbers] = useState([]);
+
+  const handleClick = (num) => {
+    if (selectedNumbers.includes(num)) {
+      // Deselect the number if it's already selected
+      setSelectedNumbers(
+        selectedNumbers.filter((selectedNum) => selectedNum !== num)
+      );
+    } else {
+      // Select the number if it's clicked
+      setSelectedNumbers([...selectedNumbers, num]);
+    }
+  };
 
   return (
     <div className="empty-boxes-container">
       {numbers.map((num) => {
         const isHighlighted = num === dice1 || num === dice2 || num === sum;
+        const isSelected = selectedNumbers.includes(num);
+
         return (
           <div
             key={num}
-            className={`empty-box ${isHighlighted ? 'highlighted-box' : ''}`}
+            className={`empty-box ${isHighlighted ? 'highlighted-box' : ''} ${
+              isSelected ? 'selected-box' : ''
+            }`}
+            onClick={() => handleClick(num)}
           >
-            {num}
+            {isSelected ? 'x' : num}
           </div>
         );
       })}
